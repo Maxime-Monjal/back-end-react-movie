f902367f3b38067a24993d2556dd7bc6ab89871d
Maxime Monjal <maxime.monjal@gmail.com>
2023-11-08 21:33:33 +0100

    clean some stuff

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ const API_SECRET = process.env.API_SECRET
 server.get("/", async (request, reply) => {
   const page = 1
   try {
-    const movies = await fetch(`${BASE_URL}movie/popular?api_key=${API_SECRET}&language=fr&page=${page}`).then((res) => res.json())
+    const url = `${BASE_URL}movie/popular?api_key=${API_SECRET}&language=fr&page=${page}`
+    const options = {method: 'GET', headers: {accept: 'application/json'}}
+    const movies = await fetch(url, options).then((res) => res.json())
 
     reply.status(200)
     reply.send(movies.results)
@@ -33,7 +35,9 @@ server.get("/", async (request, reply) => {
 server.get("/toprated/:page", async (request, reply) => {
   const page = request.params.page ?? 1
   try {
-    const topRatedMovies = await fetch(`${BASE_URL}movie/top_rated?api_key=${API_SECRET}&language=fr&page=${page}`).then((res) => res.json())
+    const url = `${BASE_URL}movie/top_rated?api_key=${API_SECRET}&language=fr&page=${page}`
+    const options = {method: 'GET', headers: {accept: 'application/json'}}
+    const topRatedMovies = await fetch(url, options).then((res) => res.json())
 
     reply.status(200)
     reply.send(topRatedMovies.results)
@@ -47,7 +51,9 @@ server.get("/toprated/:page", async (request, reply) => {
 server.get("/upcoming/:page", async (request, reply) => {
   const page = request.params.page ?? 1
   try {
-    const upComingMovies = await fetch(`${BASE_URL}movie/upcoming?api_key=${API_SECRET}&language=fr&page=${page}`).then((res) => res.json())
+    const url = `${BASE_URL}movie/upcoming?api_key=${API_SECRET}&language=fr&page=${page}`
+    const options = {method: 'GET', headers: {accept: 'application/json'}}
+    const upComingMovies = await fetch(url, options).then((res) => res.json())
     
     reply.status(200)
     reply.send(upComingMovies.results)
@@ -61,7 +67,9 @@ server.get("/upcoming/:page", async (request, reply) => {
 server.get("/actors/:id", async (request, reply) => {
   const actorId = request.params.id 
   try {
-    const actors = await fetch(`${BASE_URL}movie/${actorId}/credits?api_key=${API_SECRET}&language=fr`).then((res) => res.json())
+    const url = `${BASE_URL}movie/${actorId}/credits?api_key=${API_SECRET}&language=fr`
+    const options = {method: 'GET', headers: {accept: 'application/json'}}
+    const actors = await fetch(url, options).then((res) => res.json())
 
     reply.status(200)
     reply.send(actors.cast)
@@ -75,11 +83,15 @@ server.get("/actors/:id", async (request, reply) => {
 server.get("/actor/:id", async (request, reply) => {
   const actorId = request.params.id 
   try {
+    const urlActor = `${BASE_URL}person/${actorId}?api_key=${API_SECRET}&language=fr`
+    const urlExternalIds = `${BASE_URL}person/${actorId}/external_ids?api_key=${API_SECRET}&language=fr`
+    const urlMovieCredits = `${BASE_URL}person/${actorId}/movie_credits?api_key=${API_SECRET}&language=fr`
+    const options = {method: 'GET', headers: {accept: 'application/json'}}
     const actorDetail = await Promise.all(
       [
-        fetch(`${BASE_URL}person/${actorId}?api_key=${API_SECRET}&language=fr`).then((res) => res.json()),
-        fetch(`${BASE_URL}person/${actorId}/external_ids?api_key=${API_SECRET}&language=fr`).then((res) => res.json()),
-        fetch(`${BASE_URL}person/${actorId}/movie_credits?api_key=${API_SECRET}&language=fr`).then((res) => res.json()),
+        fetch(urlActor, options).then((res) => res.json()),
+        fetch(urlExternalIds, options).then((res) => res.json()),
+        fetch(urlMovieCredits, options).then((res) => res.json()),
       ]
     )
 
@@ -96,7 +108,9 @@ server.get("/similar/:id/:page", async (request, reply) => {
   const movieId = request.params.id
   const page = request.params.page ?? 1
   try {
-    const similarMovies = await fetch(`${BASE_URL}movie/${movieId}/similar?api_key=${API_SECRET}&language=fr&page=${page}`).then((res) => res.json())
+    const url = `${BASE_URL}movie/${movieId}/similar?api_key=${API_SECRET}&language=fr&page=${page}`
+    const options = {method: 'GET', headers: {accept: 'application/json'}}
+    const similarMovies = await fetch(url, options).then((res) => res.json())
 
     reply.status(200)
     reply.send(similarMovies.results)
@@ -110,7 +124,9 @@ server.get("/similar/:id/:page", async (request, reply) => {
 server.get("/movie/:id", async (request, reply) => {
   const movieId = request.params.id
   try {
-    const movie = await fetch(`${BASE_URL}movie/${movieId}?api_key=${API_SECRET}&language=fr`).then((res) => res.json())
+    const url = `${BASE_URL}movie/${movieId}?api_key=${API_SECRET}&language=fr`
+    const options = {method: 'GET', headers: {accept: 'application/json'}}
+    const movie = await fetch(url, options).then((res) => res.json())
 
     reply.status(200)
     reply.send(movie)
@@ -125,7 +141,9 @@ server.get("/movie/search/:query/:page", async (request, reply) => {
   const query = request.params.query ?? ""
   const page = request.params.page ?? 1
   try {
-    const searchedMovie = await fetch(`${BASE_URL}search/movie?api_key=${API_SECRET}&language=fr&query=${query}&page=${page}`).then((res) => res.json())
+    const url = `${BASE_URL}search/movie?api_key=${API_SECRET}&language=fr&query=${query}&page=${page}`
+    const options = {method: 'GET', headers: {accept: 'application/json'}}
+    const searchedMovie = await fetch(url, options).then((res) => res.json())
 
     reply.status(200)
     reply.send(searchedMovie.results)
